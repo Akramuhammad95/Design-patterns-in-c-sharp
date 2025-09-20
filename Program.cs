9cd9f079300ff2b50424d078c412b084d24c2f57
Akram Muhammad <akramuhammad95@outlook.com>
2025-09-20 12:55:46 +0300

    alter  name of the class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using system;
+
+namespace dp
+
+{
+    // singelton pattern
+
+    class Database
+    //Developers use the Singleton Pattern to manage shared resources.
+    //  For example,
+    //  a database class can implement the Singleton Pattern to ensure:
+    //  there is only one connection to the database throughout the application
+    {
+        private static Database _instance;
+        //constructor
+        private DataBase() { }
+        //method to get the instance
+        public static Database getInstance()
+        {
+            if (_instance == null)
+            {
+                //create the new and only instance if it doesn't exist
+                _instance = new DataBase();
+                
+            }
+            return instance;
+        }
+        
+
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    
+}
