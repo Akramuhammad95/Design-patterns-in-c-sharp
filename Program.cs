575dbb10b322914e369b479b55342961829b5bbc
Akram Muhammad <akramuhammad95@outlook.com>
2025-09-20 16:31:44 +0300

    rest of patterns

--- Program.cs
+++ Program.cs
@@ -25,11 +25,132 @@ namespace dp
             }
             return instance;
         }
-        
-
-
-
     }
+    // ===================factory pattern==================
+    interface iNotification
+    //to create objects without exposing the instantiation logic to the client
+    {
+        void notifyUser();
+    }
+    class SMSNotification : iNotification
+    {
+        public void notifyUser()
+        {
+            Console.WriteLine("Sending SMS notification");
+        }
+    }
+
+    class EmailNotification : iNotification
+    {
+        public void notifyUser()
+        {
+            Console.WriteLine("Sending Email notification");
+        }
+    }
+
+    class FactoryNotification
+    {
+        //factory method to create objects based on the channel type
+        public iNotification createNotificationFactory(string channel)
+        {
+            if (channel == "SMS")
+            {
+                return new SMSNotification();
+            }
+            else if (channel == "Email")
+            {
+                return new EmailNotification();
+            }
+            else
+            {
+                throw new ArgumentException("Invalid channel");
+            }
+        }
+    }
+
+    //==================observer pattern==================
+    interface IObserver
+    {
+        void update(string message);
+    }
+    public class weatherchange
+    {
+
+        private List<IObserver> observers = new List<IObserver>();
+        public void addObserver(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+        public void removeObserver(IObserver observer)
+        {
+            observers.Remove(observer);
+        }
+        public void notifyObservers(string message)
+        {
+            foreach (var observer in observers)
+            {
+                observer.update(message);
+            }
+        }
+    }
+    public class DesktopWeatherApp : IObserver
+    {
+        private string appName;
+        public WeatherApp(string name)
+        {
+            DesktopWeatherApp = name;
+        }
+        public void update(string message)
+        {
+            Console.WriteLine($"{appName} received weather update: {message}");
+        }
+    }
+    public class MobileWeatherApp : IObserver
+    {
+        private string appName;
+        public MobileWeatherApp(string name)
+        {
+            MobileWeatherApp = name;
+        }
+        public void update(string message)
+        {
+            Console.WriteLine($"{appName} received weather update: {message}");
+        }
+    }
+
+    //==============================adapter pattern==============================================//
+    interface ITarget
+    {
+        string getRequest();
+    }
+
+
+
+    class Adaptee
+    {
+        public string getSpecificRequest()
+        {
+            return "Specific request";
+        }
+    }
+
+    class Adapter : ITarget
+    {
+        private Adaptee _adaptee;
+        public Adapter(Adaptee adaptee)
+        {
+            _adaptee = adaptee;
+        }
+        public string getRequest()
+        {
+            return _adaptee.getSpecificRequest();
+        }
+    }
+
+
+    //==============================end of adapter pattern==============================================//
+
+
     class Program
     {
         static void Main(string[] args)
